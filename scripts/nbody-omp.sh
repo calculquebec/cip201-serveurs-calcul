0c9c3c635b7ad904c4b111a2c415eb0aa610c7bc
Pier-Luc St-Onge <pier-luc.st-onge@calculquebec.ca>
2021-09-02 20:44:28 -0400

    Exercice de pipeline et post-traitement

---
+++ scripts/nbody-omp.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --mem-per-cpu=2000M
+#SBATCH --time=0-00:10
+
+module load gcc
+
+cd $SLURM_TMPDIR
+cp $SCRATCH/cq-formation-nbody/solutions/openmp/{nbody_omp,parameters.txt} ./
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+duree=$(2>&1 time -p ./nbody_omp parameters.txt | grep -E '^real')
+duree=$(echo $duree | cut -d' ' -f2)
+
+md5sum -c $SCRATCH/cq-formation-nbody/solutions/md5/rkutta_parameters_txt.md5
+echo "Temps écoulé: $duree sec. avec $OMP_NUM_THREADS processeur(s)"
---
+++ scripts/pipeline-nbody.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ "$(basename $PWD)" = "scripts" ]; then
+  cd ..
+fi
+
+if [ ! -z "$ACCOUNT" ]; then
+  ACCOUNT="--account=$ACCOUNT"
+fi
+
+RESULTATS=/scratch/$USER/resultats
+mkdir -p $RESULTATS
+
+for cpus in 1 2 4 8; do
+  sbatch $ACCOUNT --cpus-per-task=$cpus --output=$RESULTATS/resultat-$cpus.log \
+    scripts/nbody-omp.sh
+done
