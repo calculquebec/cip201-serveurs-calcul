a1e8dcc3313e0a12b522b5623e0fea5373b92cb6
Pier-Luc St-Onge <pier-luc.st-onge@calculquebec.ca>
2021-10-18 16:56:07 -0400

    Utiliser l'exemple CUDA matrixMul à la place

---
+++ scripts/cuda-matmul.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --gres=gpu:1
+#SBATCH --mem-per-cpu=2000M
+#SBATCH --time=0-00:10
+
+module load gcc cuda
+
+export Dim=5760
+
+~/cuda-samples-11.0/bin/x86_64/linux/release/matrixMul \
+  -wA=$Dim -hA=$Dim -wB=$Dim -hB=$Dim
